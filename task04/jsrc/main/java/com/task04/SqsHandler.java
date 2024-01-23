908fe7db4b11c945dcf20753c8588c3789c1e5ef
ab090137 <artem_belmeha@epam.com>
2024-01-23 17:44:57 +0200

    init

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -7,6 +7,9 @@ import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
 
 @LambdaHandler(lambdaName = "sqs_handler",
 	roleName = "sqs_handler-role",
@@ -20,7 +23,8 @@ public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 		for (SQSMessage msg : event.getRecords()) {
 		String message = msg.getBody();
 		// Print the message content to CloudWatch
-		System.out.println("Message content: " + message);
+			context.getLogger().log("Message received: " + message);
+			System.out.println("Message content: " + message);
 		}
 		return null;
 	}
